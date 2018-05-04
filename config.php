1182d025f90566e97224a9763225e97d5434b69c
Roman Lytvynenko <litvinenko95@gmail.com>
2018-05-04 16:03:36 +0300

    vagrant and phing build

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ return [
         'holder_template' => '<div data-eddbk-widget-id="%s" data-config=\'%s\'></div>',
         'style' => plugins_url(RC_WP_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/dist/app.css', EDDBK_FILE),
         'script' => plugins_url(RC_WP_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/dist/app.js', EDDBK_FILE),
-        'main_script' => plugins_url(RC_WP_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/dist/main.js', EDDBK_FILE),
+        'main_script' => plugins_url(RC_WP_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/assets/main.js', EDDBK_FILE),
     ]
 ];
