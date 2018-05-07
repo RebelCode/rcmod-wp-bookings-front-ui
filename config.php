8d15cacd19f900bc34520aec9954dff437f463d8
Roman Lytvynenko <litvinenko95@gmail.com>
2018-05-07 20:21:40 +0300

    use script and style from CDN

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 return [
     'bookings_front_ui' => [
         'holder_template' => '<div data-eddbk-widget-id="%s" data-config=\'%s\'></div>',
-        'style' => plugins_url(RC_WP_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/dist/app.css', EDDBK_FILE),
-        'script' => plugins_url(RC_WP_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/dist/app.js', EDDBK_FILE),
+        'style' => 'https://unpkg.com/@rebelcode/bookings-client@0.0.1/dist/static/css/app.css',
+        'script' => 'https://unpkg.com/@rebelcode/bookings-client@0.0.1/dist/static/js/app.js',
         'main_script' => plugins_url(RC_WP_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/assets/main.js', EDDBK_FILE),
     ]
 ];
