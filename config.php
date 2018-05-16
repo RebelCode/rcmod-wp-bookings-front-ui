796137e135f12b863b575bab98a84e8bca540ae1
Roman Lytvynenko <litvinenko95@gmail.com>
2018-05-16 18:22:56 +0300

    bump assets versions

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 return [
     'bookings_front_ui' => [
         'holder_template' => '<div data-eddbk-widget-id="%s" data-config=\'%s\'></div>',
-        'style' => 'https://unpkg.com/@rebelcode/bookings-client@0.0.1/dist/static/css/app.css',
-        'script' => 'https://unpkg.com/@rebelcode/bookings-client@0.0.1/dist/static/js/app.js',
+        'style' => 'https://unpkg.com/@rebelcode/bookings-client@0.0.2/dist/static/css/app.css',
+        'script' => 'https://unpkg.com/@rebelcode/bookings-client@0.0.2/dist/static/js/app.js',
         'main_script' => plugins_url(RC_WP_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/assets/main.js', EDDBK_FILE),
     ]
 ];
