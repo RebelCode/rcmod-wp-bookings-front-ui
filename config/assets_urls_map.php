3da8ca6df072465f58957e3ce05e3bf209bafcd3
Roman Lytvynenko <litvinenko95@gmail.com>
2018-09-13 14:02:09 +0300

    pass week starts on setting to wizard application
    EDDBK-93

--- config/assets_urls_map.php
+++ config/assets_urls_map.php
@@ -3,8 +3,8 @@
 return [
     'require' => 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.5/require.js',
 
-    'wizard_ui/dist/app.min.js' => 'https://unpkg.com/@rebelcode/booking-wizard@0.1.4/dist/js/app.min',
-    'wizard_ui/assets/main.js'  => plugins_url(RC_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/dist/main.js', EDDBK_FILE),
+    'wizard_ui/dist/app.min.js' => 'https://unpkg.com/@rebelcode/booking-wizard@0.1.5/dist/js/app.min',
+    'wizard_ui/dist/main.js'  => plugins_url(RC_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/dist/main.js', EDDBK_FILE),
 
     'wizard_ui/dist/wp-bookings-front-ui.css' => plugins_url(RC_BOOKINGS_FRONT_UI_MODULE_RELATIVE_DIR . '/dist/wp-bookings-front-ui.css', EDDBK_FILE),
 ];
